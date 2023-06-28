5dcca2dd0871359ebec8bf1f8478e47d506fc405
rohianagoyal <122933466+rohianagoyal@users.noreply.github.com>
2023-06-28 17:41:55 +0530

    Delete dba-to-de directory

--- dba-to-de/src/main/scala/io/datatroops/Hello.scala
+++
@@ -1,5 +0,0 @@
-package io.datatroops
-
-object Hello extends App {
-  println("Hello")
-}
--- dba-to-de/src/test/scala/io/datatroops/HelloSpec.scala
+++
@@ -1,7 +0,0 @@
-package io.datatroops
-
-class HelloSpec extends munit.FunSuite {
-  test("say hello") {
-    assertEquals(Hello.greeting, "hello")
-  }
-}
